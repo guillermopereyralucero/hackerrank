70ae5436e895743c4690c518cc7bdf74f31ee4f7
Guillermo Pereyra Lucero <99100769+guillermopereyralucero@users.noreply.github.com>
2024-07-10 21:10:19 +0200

    Create WeatherObservationStation13.sql

---
+++ SQL/Aggregation/WeatherObservationStation13/WeatherObservationStation13.sql
@@ -0,0 +1,11 @@
+/*Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than  and less than . Truncate your answer to  decimal places.
+
+Input Format
+
+The STATION table is described as follows:
+
+Station.jpg
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.*/
+
+
